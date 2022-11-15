1ab33375fec5ddbd8a3cd3c5dd813966d72c20ca
Catherine Miguel <catmig1992@gmail.com>
2022-11-15 12:33:10 -0800

    Added the edit functionality but does not redirect to show page

--- controllers/places.js
+++ controllers/places.js
@@ -49,10 +49,6 @@ router.get("/:id/edit", (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
-  res.send("STUB EDITED /places");
-});
-
 //DELETE
 router.delete("/:id", (req, res) => {
   let id = Number(req.params.id);
@@ -63,6 +59,31 @@ router.delete("/:id", (req, res) => {
   } else {
     places.splice(id, 1);
     res.redirect("/places");
+  }
+});
+
+router.put("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    // Dig into req.body and make sure data is valid
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = "http://placekitten.com/400/400";
+    }
+    if (!req.body.city) {
+      req.body.city = "Anytown";
+    }
+    if (!req.body.state) {
+      req.body.state = "USA";
+    }
+
+    // Save the new data into places[id]
+    places[id] = req.body;
+    res.redirect(`/places/${id}`);
   }
 });
 
