f31488ba175f9738bfed065f901646b98d72effc
Catherine Miguel <catmig1992@gmail.com>
2022-11-29 19:56:30 -0800

    Delete and Edit bug resolved

--- controllers/places.js
+++ controllers/places.js
@@ -44,44 +44,6 @@ router.get("/:id", (req, res) => {
     });
 });
 
-//UPDATE
-router.put("/:id", (req, res) => {
-  // res.send("PUT /places/:id stub");
-  db.Place.findByIdAndUpdate(req.params.id, req.body)
-    .then(() => {
-      res.redirect(`/places/${req.params.id}`);
-    })
-    .catch((err) => {
-      console.log("err", err);
-      res.render("error404");
-    });
-});
-
-//DELETE
-router.delete("/:id", (req, res) => {
-  //   // res.send("DELETE /places/:id stub");
-  db.Place.findByIdAndDelete(req.params.id)
-    .then((place) => {
-      res.redirect("/places");
-    })
-    .catch((err) => {
-      console.log("err", err);
-      res.render("error404");
-    });
-});
-
-//EDIT
-router.get("/:id/edit", (req, res) => {
-  // res.send("GET edit form stub");
-  db.Place.findById(req.params.id)
-    .then((place) => {
-      res.render("places/edit", { place });
-    })
-    .catch((err) => {
-      res.render("error404");
-    });
-});
-
 //COMMENT
 router.post("/:id/comment", (req, res) => {
   console.log(req.body);
@@ -104,70 +66,39 @@ router.post("/:id/comment", (req, res) => {
     });
 });
 
-router.post("/:id/rant", (req, res) => {
-  res.send("GET /places/:id/rant stub");
+//EDIT
+router.get("/:id/edit", (req, res) => {
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      res.render("places/edit", { place });
+    })
+    .catch((err) => {
+      res.render("error404");
+    });
 });
 
-router.delete("/:id/rant/:rantId", (req, res) => {
-  res.send("GET /places/:id/rant/:rantId stub");
+//UPDATE
+router.put("/:id", (req, res) => {
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.redirect(`/places/${req.params.id}`);
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+});
+
+//DELETE
+router.delete("/:id", (req, res) => {
+  db.Place.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.redirect("/places");
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
 });
 
 module.exports = router;
-
-// //EDIT
-// router.get("/:id/edit", (req, res) => {
-//   let id = Number(req.params.id);
-//   if (isNaN(id)) {
-//     res.render("error404");
-//   } else if (!places[id]) {
-//     res.render("error404");
-//   } else {
-//     res.render("places/edit", { place: places[id], id });
-//   }
-// });
-
-// //UPDATE
-// router.put("/:id", (req, res) => {
-//   let id = Number(req.params.id);
-//   if (isNaN(id)) {
-//     res.render("error404");
-//   } else if (!places[id]) {
-//     res.render("error404");
-//   } else {
-//     // Dig into req.body and make sure data is valid
-//     if (!req.body.pic) {
-//       // Default image if one is not provided
-//       req.body.pic = "http://placekitten.com/400/400";
-//     }
-//     if (!req.body.city) {
-//       req.body.city = "Anytown";
-//     }
-//     if (!req.body.state) {
-//       req.body.state = "USA";
-//     }
-
-//     // Save the new data into places[id]
-//     places[id] = req.body;
-//     res.redirect(`/places/${id}`);
-//   }
-// });
-
-// router.post("/:id", (req, res) => {
-//   let id = Number(req.params.id);
-//   res.redirect(`/places/${id}`);
-// });
-
-// //DELETE
-// router.delete("/:id", (req, res) => {
-//   let id = Number(req.params.id);
-//   if (isNaN(id)) {
-//     res.render("error404");
-//   } else if (!places[id]) {
-//     res.render("error404");
-//   } else {
-//     places.splice(id, 1);
-//     res.redirect("/places");
-//   }
-// });
-
-// module.exports = router;
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -15,7 +15,7 @@ function show(data) {
     for (let i = 0; i < averageRating; i++) {
       stars += "⭐";
     }
-    rating = <h3>{stars} stars</h3>;
+    rating = <h3>{stars}</h3>;
     comments = data.place.comments.map((c) => {
       return (
         <div className="border">
@@ -47,10 +47,16 @@ function show(data) {
             <h2>Description</h2>
             <h3>{data.place.showEstablished()}</h3>
             <h4>Serving {data.place.cuisines}</h4>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+            <a
+              href={`/places/${data.place.id}/edit`}
+              className="btn btn-warning"
+            >
               Edit
             </a>
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+            <form
+              method="POST"
+              action={`/places/${data.place.id}?_method=DELETE`}
+            >
               <button type="submit" className="btn btn-danger">
                 Delete
               </button>
