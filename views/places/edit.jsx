8ae7500cc2fa0e1bc729bff1b7ced27b78016906
Catherine Miguel <catmig1992@gmail.com>
2022-11-29 18:03:54 -0800

    Updated the delete, edit and update routers

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -59,7 +59,20 @@ function edit_form(data) {
               required
             />
           </div>
-          <input className="btn btn-primary" type="submit" value="Add Place" />
+          <div className="form-group col-sm-4">
+            <label htmlFor="founded">Founded</label>
+            <input
+              className="form-control"
+              id="founded"
+              name="founded"
+              value={data.place.founded}
+            />
+          </div>
+          <input
+            className="btn btn-primary"
+            type="submit"
+            value="Update Place"
+          />
         </form>
       </main>
     </Def>
