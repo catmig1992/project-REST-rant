a980089eb942d1e9353a9e4d412ee6b8c3698b16
Catherine Miguel <catmig1992@gmail.com>
2022-11-29 17:30:07 -0800

    Changed the numerical rating to star emoji rating display

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,9 +8,14 @@ function show(data) {
     let sumRatings = data.place.comments.reduce((tot, c) => {
       return tot + c.stars;
     }, 0);
-    let averageRating = sumRatings / data.place.comments.length;
-    rating = <h3>{Math.round(averageRating)} stars</h3>;
-
+    // let averageRating = sumRatings / data.place.comments.length;
+    // rating = <h3>{Math.round(averageRating)} stars</h3>;
+    let averageRating = Math.round(sumRatings / data.place.comments.length);
+    let stars = " ";
+    for (let i = 0; i < averageRating; i++) {
+      stars += "⭐";
+    }
+    rating = <h3>{stars} stars</h3>;
     comments = data.place.comments.map((c) => {
       return (
         <div className="border">
